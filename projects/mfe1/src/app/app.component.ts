2713b3970b7cca0b3647da34ece03067f8cc0f47
msscsh <itsbruno42@gmail.com>
2025-03-30 16:48:24 -0300

    protecting mfe1 with keycloak

--- projects/mfe1/src/app/app.component.ts
+++ projects/mfe1/src/app/app.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { KeycloakService } from 'keycloak-angular';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'mfe1';
+  identityservice = inject(KeycloakService);
+  logout = ()=>{
+    this.identityservice.logout();
+   }
 }
--- projects/mfe1/src/app/app.config.ts
+++ projects/mfe1/src/app/app.config.ts
@@ -1,8 +1,41 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { KeycloakService } from 'keycloak-angular';
 import { routes } from './app.routes';
+import { KeycloakConfig } from './config/keycloak.config';
 
+function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak
+      .init({
+        config:KeycloakConfig,
+        initOptions: {
+          onLoad: 'login-required',
+          checkLoginIframe: false,
+        },
+        enableBearerInterceptor: true,
+      })
+      .then(async (authenticated) => {
+        if (authenticated) {
+          localStorage.setItem('Token', await keycloak.getToken());
+        } else {
+          console.warn('User not authenticated');
+        }
+      })
+      .catch((error) => {
+        console.error('Keycloak initialization failed:', error);
+      });
+}
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService],
+    },
+    KeycloakService,
+  ]
 };
